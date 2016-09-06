b7eacf6ba9aec7d0376153f6b697cdd93fc51215
priya1316 <priya.pm2@gmail.com>
2016-09-06 08:23:33 -0700

    Added Files

---
+++ APLesson_01/ASCIIArt.java
@@ -0,0 +1,22 @@
+public class ASCIIArt
+{
+	public static void main(String[]args)
+	{
+		System.out.println("  @ .. @");
+		System.out.println("  (----)" );
+		System.out.println(" (> ___<)");
+		System.out.println(" ^^ ~~ ^^");
+		
+		System.out.println("    ,____");
+		System.out.println("    /   /^\\  ,____");
+		System.out.println("    \\__),  ^^   _, \\");
+		System.out.println("        / @   @ \\\\_/");
+		System.out.println("      __{_-.X.-_}__");
+		System.out.println("   (________________)");
+		System.out.println("        |      |");
+		System.out.print("        [______]");
+	
+		
+	}
+	
+}
