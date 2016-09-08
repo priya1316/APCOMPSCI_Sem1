f369be57fc29f8619eedf1cd96e62b1ee0cf75cc
priya1316 <priya.pm2@gmail.com>
2016-09-08 08:40:12 -0700

    Finished Lab_02

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,27 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 5;
+		int b = 4;
+		int c = a*b;
+		
+		System.out.println("Nine multiplied by eight is" + " " + c);
+	
+	
+		int num = 6293;
+		int zip = 92130;
+		
+		System.out.println(num + " " + "Belmont Trail Ct. San Diego, CA" + " " + zip);
+		
+		int l = 6;
+		int w = 3;
+		int h = 2;
+		int lw = 2*l*w;
+		int lh = 2*l*h;
+		int wh = 2*w*h;
+		int sufArea = lw + lh + wh;
+		
+		System.out.println("The surface area of your rectangle is" + " " + sufArea);
+	}
+}
