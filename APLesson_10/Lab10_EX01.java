4897f96f1a850e718a0ac8583c2f392661dc507b
priya1316 <priya.pm2@gmail.com>
2016-12-05 09:17:33 -0800

    added files

--- APLesson_10/Lab10_EX01.java
+++ APLesson_10/Lab10_EX01.java
@@ -4,23 +4,36 @@ import java.util.Arrays;
 
 public class Lab10_EX01
 {
+	static ArrayList<Integer>nums;
 	public static void main(String[]args)
 	{
-		Integer numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
-		ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(numbers));
-		
-		gFactor();
+		Integer [] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
+		nums = new ArrayList<>(Arrays.asList(numbers));
 		removePrimes();
+		System.out.println(nums);
 	}
 	
-	public static int gFactor(int [] n)
+	public static int gFactor(int n)
 	{
-		for(int i = 0; i <= n.length; i++ )
+		for(int i = 2; i < n; i++)
 		{
-			if (n%number == 0)
-				return 1
-			else
-				return 0
+			if (n % i == 0)
+			{
+				return 1;
+			}	
+			
+		}
+		return 0;
+	}
+	public static void removePrimes()
+	{
+		for(int i = 0; i < nums.size(); i++)
+		{
+			if(gFactor(nums.get(i)) == 0)
+			{
+				nums.remove(i);
+				i--;
+			}
 		}
 	}
 }
---
+++ APLesson_10/Lab10_EX02.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays; 
+
+public class Lab10_EX02
+{
+	static ArrayList<String>equation;
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter an equation: ");
+		String expression = kb.nextLine();
+		
+		equation = new ArrayList<>(Arrays.asList(expression.split(" ")));
+		
+		doEquation();
+		System.out.println(equation);
+
+	}
+	public static void doEquation()
+	{
+		int i = 0;
+		while(i < equation.size())
+		{
+			if(equation.get(i).equals("*") || equation.get(i).equals("/"))
+			{
+				if(equation.get(i).equals("*"))
+				{
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) * Integer.parseInt(equation.get(i+1))));
+				}
+				else
+				{
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) / Integer.parseInt(equation.get(i+1))));
+				}
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else
+			{
+				i++;
+			}
+		}
+		while(i < equation.size())
+		{
+			if(equation.get(i).equals("+") || equation.get(i).equals("-"))
+			{
+				if(equation.get(i).equals("+"))
+				{
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + Integer.parseInt(equation.get(i+1))));
+				}
+				else
+				{
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) - Integer.parseInt(equation.get(i+1))));
+				}
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else 
+			{
+				i++;
+			}
+		}
+		
+	}
+}
