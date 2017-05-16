598daa2e9f58f1db671305a08406a914f049ac73
priya1316 <priya.pm2@gmail.com>
2017-05-16 09:16:41 -0700

    added files

---
+++ CompsciProject/ASCIIArt.java
@@ -0,0 +1,27 @@
+public class ASCIIArt
+{
+public static void main (String[]args)
+	{
+		System.out.println("         /////////////\\\\");
+		System.out.println("       (((((((((((((( \\\\");
+		System.out.println("       ))) ~~      ~~  (((");
+		System.out.println("       ((( (*)     (*) )))");
+		System.out.println("       )))     <       (((");
+		System.out.println("       ((( '\\______/`  )))");
+		System.out.println("       )))\\___________/(((");
+		System.out.println("       )))             (((");
+		System.out.println("       )))             (((");
+		
+	
+	
+		System.out.println("    /////////////\\\\");
+		System.out.println("  (((((((((((((( \\\\");
+		System.out.println("  ))) ~~      ~~  (((");
+		System.out.println("  \\  (*)     (*)   /");
+		System.out.println("   \\      <       /");
+		System.out.println("    \\ '\\______/` / ");
+		System.out.println("     \\__________/");
+		
+	}
+	
+}
---
+++ CompsciProject/MysteryMaze.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class MysteryMaze
+{
+	static int LIVES = 3
+	
+	public static String character()
+	{
+		System.out.println("Choose your character!");
+		int character = kb.next();
+		if(response.equals("Gertrude")
+		{
+			System.out.println("You picked Gertrude! Great choice!");
+		}
+		if else(response.equals("Billy Bob Joe")
+		{
+			System.out.println("You picked Billy Bob Joe! Awesome!");
+		}
+		else
+		{
+			System.out.println("Please pick a character.");
+		}
+	}
+	public static void lives()
+	{
+		if(!response.equals(response))
+		{
+			LIVES--;
+		}
+	}
+	public static void AddLife()
+	{
+		for(int i = 0; i = question.length(); i++)
+		{
+			if(question[i]/4 == 0)
+			{
+				System.out.println("Power up! You get an extra life!")
+				LIVES++; 
+			}
+		}
+	}
+}
