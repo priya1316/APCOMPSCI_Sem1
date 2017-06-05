32a3f4f4a3a384178d80b394541d27e97da8ddda
priya1316 <priya.pm2@gmail.com>
2017-06-05 13:29:10 -0700

    Update IntArrayWorkerTester.java

--- PictureLab_Student/classes/IntArrayWorkerTester.java
+++ PictureLab_Student/classes/IntArrayWorkerTester.java
@@ -95,10 +95,10 @@ public class IntArrayWorkerTester
   {
     testSetMatrix();
     testFillPattern1();
-    //testGetCount(); 
+    testGetCount(); 
     testGetTotal();
     testGetTotalNested();
-    //testGetLargest();
-    //testGetColTotal();
+    testGetLargest();
+    testGetColTotal();
   }
 }
