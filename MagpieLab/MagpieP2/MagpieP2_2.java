135f950be5cca069e28b7d79fd6aeede1009e3f6
priya1316 <priya.pm2@gmail.com>
2017-06-07 07:58:39 -0700

    Update MagpieP2_2.java

--- MagpieLab/MagpieP2/MagpieP2_2.java
+++ MagpieLab/MagpieP2/MagpieP2_2.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class MagpieP2_2
 {
 	/** Get a default greeting @return a greeting*/
@@ -19,69 +18,59 @@ public class MagpieP2_2
 		
 		if(statement.length() == 0)
 		{
-			return "Say something, please.";
+			return "Say something, please";
 		}
 
-		/** Exercise_01:
-		 * ==================================================
-		 * 	Code that asks the user "Say something, please."
-		 * 	if you enter nothing, or if you accidentally hit
-		 * 	enter. Think to yourself: "What is the length of
-		 * 	an empty String?" */
-
-
-		/** To be completed in Exercise_02:
-		 * 	Modify the following code to use the findKeyword
-		 * 	Method (details in "Exercise_02" below. */
-		if (findKeyword(statement, "no", 0) >= 0)
+		if(findKeyword(statement, "no", 0) >= 0)
 		{
 			response = "Why so negative?";
 		}
 
-		else if (findKeyword(statement, "mother", 0) >= 0
-				|| findKeyword(statement,"father", 0) >= 0
-				|| findKeyword(statement,"sister", 0) >= 0
-				|| findKeyword(statement, "brother",0) >= 0)
+		else if(findKeyword(statement, "mother", 0) >= 0
+				|| findKeyword(statement, "father", 0) >= 0
+				|| findKeyword(statement, "sister", 0) >= 0
+				|| findKeyword(statement, "brother", 0) >= 0)
 		{
 			response = "Tell me more about your family.";
 		}
-		else if (findKeyword(statement, "cat", 0) >= 0
-				|| findKeyword(statement,"dog", 0) >= 0
-				|| findKeyword(statement,"bird", 0) >= 0
-				|| findKeyword(statement, "fish",0) >= 0)
+		
+		else if(findKeyword(statement, "cat", 0) >= 0
+				|| findKeyword(statement, "dog", 0) >= 0
+				|| findKeyword(statement, "fish", 0) >= 0
+				|| findKeyword(statement, "turtle", 0) >= 0)
 		{
-			response = "Tell me more about your pet!";
-		}
-		else if(findKeyword(statement, "Smith", 0) >=0)
-		{
-			response = "She sounds like a pretty great teacher!";
+			response = "Tell me more about your pet.";		
 		}
 
-		//responses that require transformations
-		else if (findKeyword(statement, "I want to" , 0) >=0)
+		else if(findKeyword(statement, "Robinette", 0) >= 0)
+		{
+			response = "He sounds like a pretty dank teacher.";
+		}
+		
+		//Resonses which require transformations
+		else if(findKeyword(statement, "I want to", 0) >= 0)
 		{
 			response = transformIWantToStatement(statement);
 		}
-		else
+		
+		else 
 		{
 			//Look for a two word (you <something> me)
 			//pattern
-			
 			int psn = findKeyword(statement, "you", 0);
-			
-			if (psn >= 0 && findKeyword(statement, "me", psn) >=0)
+		
+		
+			if(psn >= 0 && findKeyword(statement, "me", psn) >= 0)
 			{
 				response = transformYouMeStatement(statement);
 			}
 			else
 			{
 				response = getRandomResponse();
-			}	
-			
-		}
-		return response;
+			} 
+		}	
+			return response;
 	}
-
 	
 	private String transformIWantToStatement(String statement)
 	{
@@ -91,12 +80,12 @@ public class MagpieP2_2
 		{
 			statement = statement.substring(0, statement.length() -1);
 		}
-		int psn = findKeyword(statement, "I want to");
-		String restOfStatement= statement.substring(psn + 9);
-		return "What would it mean to" + restOfStatement + "?";
-	
-	}
 		
+		int psn = findKeyword(statement, "I want to");
+		String restOfStatement = statement.substring(psn + 9);
+		return "What would it mean to" + restOfStatement + "?";
+	}
+	
 	private String transformYouMeStatement(String statement)
 	{
 		statement = statement.trim();
@@ -105,62 +94,66 @@ public class MagpieP2_2
 		{
 			statement = statement.substring(0, statement.length() -1);
 		}
+		
 		int psnOfYou = findKeyword(statement, "you");
 		int psnOfMe = findKeyword(statement, "me", psnOfYou + 3);
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe);
 		return "What makes you think that I" + restOfStatement + "you?";
 	}
-		
+
 	private int findKeyword(String statement, String goal, int startPos)
 	{
+		/* New String variable phrase = a more searchable version of statement.
+		 	-Use a combination of trim() and toLowerCase() modify statement.
+		   New int variable psn = the location of goal in phrase after
+		   startPos
+			-->Refinement: Make sure we find goal by itself, and not part
+			of another word ("no" vs no in "know"). if you find an occurrence
+			of goal, make sure before and after aren't letters. */
+			
 		String phrase = statement.trim();
 		int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
-			
-		while (psn > 0)
-		{
-			String before = "";
-			String after = "";
-			
 		
-			/*As long as psn >= 0...
+		while(psn >= 0)
+		{
+			String before = " ";
+			String after = " ";
+			
+			/* As long as psn >= 0...
 				Check if psn > 0 - there is no need to check for before at the
 				beginning of the word
 					set before = the slot in phrase before psn */
-
-			if (psn > 0)
+			if(psn > 0)
 			{
 				before = phrase.substring(psn - 1, psn).toLowerCase();
 			}
 			
 			/*check if you can fit goal into the rest of phrase - no need to
-			proceed otherwise
-			set after = the slot in phrase after psn + length of goal */
-		
+				proceed otherwise
+					set after = the slot in phrase after psn + length of goal */
+
 			if(psn + goal.length() < phrase.length())
 			{
 				after = phrase.substring(psn + goal.length(), psn + goal.length() + 1).toLowerCase();
 			}
-
-				/* if before and after are not letters (compare before to "a"
+			
+			/* if before and after are not letters (compare before to "a"
 					and after to "z")
 						--return psn
-
 				Otherwise, search for goal in phrase from psn + 1 forward */
-				
-			if(((before.compareTo("a") < 0) || (before.compareTo("z" ) > 0 )) && ((after.compareTo("a" ) < 0)|| (after.compareTo("z") > 0)))
+			
+			if(((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) && ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
 			{
 				return psn;
 			}
-				
+			
 			else
 			{
 				psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
 			}
 		}
 		return -1;
-	}	
-
-	
+	}
 
 	/** Override - this method is used if there are only 2 parameters...*/
 	private int findKeyword(String statement, String goal)
