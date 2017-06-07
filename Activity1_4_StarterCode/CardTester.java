8142244e353b71b7d44afbeb04e9ccb7bad2f7b1
priya1316 <priya.pm2@gmail.com>
2017-06-07 14:42:14 -0700

    Update CardTester.java

--- Activity1_4_StarterCode/CardTester.java
+++ Activity1_4_StarterCode/CardTester.java
@@ -7,7 +7,21 @@ public class CardTester {
 	 * The main method in this class checks the Card operations for consistency.
 	 *	@param args is not used.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card one = new Card("7", "Diamonds", 3);
+		Card two = new Card("2", "Clubs", 3);
+		Card three = new Card("10", "Hearts", 3);
+		Card four = new Card("2", "Spades", 3);
+		
+		System.out.println("Does card two match card three?" + " " + two.matches(three));
+		System.out.println("Does card one match card three?" + " " + one.matches(three));
+		System.out.println("Card two:" + " " + two.toString());
+		System.out.println("Card four:" + " " + four.toString());
+		System.out.println("Suit of Card four: " + four.suit());
+
 	}
 }
+	
+
